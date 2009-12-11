b87c1c9f6feff78dfe38511cf7e4f76f5a51433d
Ben Lenarts <ben@lenarts.nl>
2009-12-11 15:57:00 -0800

    Adding the script header

--- Source/KeySequence.js
+++ Source/KeySequence.js
@@ -1,3 +1,20 @@
+/*
+---
+description: provides the Element.Events.addKeySequence function to create custom events for sequences of key presses.
+
+license: MIT-style
+
+authors:
+- Ben Lenarts
+
+requires:
+- core:1.2.4: Element.Event
+
+provides: [Element.Events.addKeySequence]
+
+...
+*/
+
 Element.Events.addKeySequence = function(name, sequence) {
   var buffer = new Array(sequence.length);
   var target = sequence.toString();
